55841f272616d4be0ae86a731915d1c0a423d2c0
icyvan <43236716+icyvan@users.noreply.github.com>
2019-03-28 09:02:47 +0300

    Update 22.lisp

--- 22.lisp
+++ 22.lisp
@@ -3,10 +3,10 @@
 
 
 (defun reverse-levels (l)
-     ((lambda (x) (setq last (cdr x))) l)
-  (if (null last)
-      l
-      (list (reverse-levels last) (car l))))
+    ((lambda (x) (setq last (cdr x))) l)
+    (if (null last)
+        l
+        (list (reverse-levels last) (car l))))
 
 	 
 (print (reverse-levels '(5 7 89 4 5 79)))
