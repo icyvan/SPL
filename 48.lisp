2514844e6ae93108d40331974a1cdf41229bb53f
icyvan <43236716+icyvan@users.noreply.github.com>
2019-04-03 22:27:10 +0300

    Update 48.lisp

--- 48.lisp
+++ 48.lisp
@@ -1,13 +1,13 @@
 ;Напишите предикат (ИМЕЕТ-СВОЙСТВО символ свойство), который 
 ;проверяет, обладает ли символ данным свойством.
 
-(setf (get 'books 'title) '(Looking for Alaska))
-(setf (get 'books 'author) '(John Green))
-(setf (get 'books 'publisher) '(Dutton Books))
-
 (defun property (x y)
     (if (eq (get x y) nil ) nil t)
     )
+
+(setf (get 'books 'title) '(Looking for Alaska))
+(setf (get 'books 'author) '(John Green))
+(setf (get 'books 'publisher) '(Dutton Books))
 
 (print (property 'books 'year))
 (print (property 'books 'author))
