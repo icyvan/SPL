e93993a1b2acfa6e30add120e3c22bfef0d97a43
icyvan <43236716+icyvan@users.noreply.github.com>
2019-04-03 22:10:44 +0300

    Update 17.lisp

--- 17.lisp
+++ 17.lisp
@@ -1,1 +1,13 @@
 ; Создайте предикат, порождающий всевозможные перестановки исходного множества.
+
+(defun permutation (lst)
+    (cond
+        ((null lst) nil)
+        ((null(cdr lst))(list lst))
+        (t (odd-elem-to-each-permutation
+            (car lst)
+            (permutation (cdr lst)))
+    )
+)
+
+(print (permutation '(f d g)))
