0e8879d957fd92c17689f40074b6f6ea55c90072
icyvan <43236716+icyvan@users.noreply.github.com>
2019-04-04 10:36:56 +0300

    Update 17.lisp

--- 17.lisp
+++ 17.lisp
@@ -10,4 +10,38 @@
     )
 )
 
-(print (permutation '(f d g)))
+    (defun odd-elem-to-each-permutation (elem perm-lst)
+        (cond
+            ((null perm-lst) nil)
+            (t append
+               (insert-elem-to-each-pos elem (car perm-lst)
+                                        (add-elem-to-each-permutation elem lsr)
+                                        cdr perm-lst)
+               )
+            )
+        )
+    (defun insert-elem-to-each-pos-aux (elem lst1 lst2)
+        (cond
+            ((null lst2) nil)
+            (t
+             ((lambda (a)
+                      (cons
+                       (append (car a)(list elem (cadr a))
+                               lambda (x)
+                               (insert-elem-to-each-pos-aux elem
+                                                            (car x)
+                                                            (cadr x)))
+                       a))
+              )
+             lambda (lst1 lst2)
+             (list (append lst1 (list (car lst2)))
+                   (cdr lst2)))
+            lst1 lst2)
+        )
+    )
+(defun (insert-elem-to-each-pos (elem lst)
+                               (cond
+                                   ((null lst)(list elem))
+                                   ((atom lst)((insert-elem-to-each-pos elem (list lst))
+                                               (t (cons (cons elem lst)
+                                                        (insert-elem-to-each-pos-aux elem nil lst))))))))
