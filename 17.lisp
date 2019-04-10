f3677c4ee0657d2051684f1afb6ac4ecdd623cb9
icyvan <43236716+icyvan@users.noreply.github.com>
2019-04-10 18:03:36 +0300

    Update 17.lisp

--- 17.lisp
+++ 17.lisp
@@ -1,47 +1,56 @@
-; Создайте предикат, порождающий всевозможные перестановки исходного множества.
+;Создайте предикат, порождающий всевозможные перестановки исходного множества
 
 (defun permutation (lst)
+    
     (cond
-        ((null lst) nil)
-        ((null(cdr lst))(list lst))
-        (t (odd-elem-to-each-permutation
-            (car lst)
-            (permutation (cdr lst)))
+        ( (null lst) nil)
+        ( (null (cdr lst) ) (list lst))
+        ( t (add-elem-to-each-permutation (car lst) (permutation (cdr lst) ) ) )
     )
 )
 
-    (defun odd-elem-to-each-permutation (elem perm-lst)
-        (cond
-            ((null perm-lst) nil)
-            (t append
-               (insert-elem-to-each-pos elem (car perm-lst)
-                                        (add-elem-to-each-permutation elem lsr)
-                                        cdr perm-lst)
-               )
-            )
+(defun add-elem-to-each-permutation (elem perm-lst)
+    
+    (cond
+        ( (null perm-lst) nil)
+        (t (append
+                (insert-elem-to-each-pos elem (car perm-lst))
+                (add-elem-to-each-permutation elem (cdr perm-lst) ) ;соединяем все перестановки в список
+) 
         )
-    (defun insert-elem-to-each-pos-aux (elem lst1 lst2)
-        (cond
-            ((null lst2) nil)
-            (t
-             ((lambda (a)
-                      (cons
-                       (append (car a)(list elem (cadr a))
-                               lambda (x)
-                               (insert-elem-to-each-pos-aux elem
-                                                            (car x)
-                                                            (cadr x)))
-                       a))
-              )
-             lambda (lst1 lst2)
-             (list (append lst1 (list (car lst2)))
-                   (cdr lst2)))
-            lst1 lst2)
-        )
+     )
+)   
+        
+(defun insert-elem-to-each-pos (elem lst)
+    
+    (cond
+        ( (null lst) (list elem) )
+        ( (atom lst) (insert-elem-to-each-pos elem (list lst) ) )
+        ( t (cons 
+             (cons elem lst) (insert-elem-to-each-pos-aux elem nil lst) ) ) 
     )
-(defun (insert-elem-to-each-pos (elem lst)
-                               (cond
-                                   ((null lst)(list elem))
-                                   ((atom lst)((insert-elem-to-each-pos elem (list lst))
-                                               (t (cons (cons elem lst)
-                                                        (insert-elem-to-each-pos-aux elem nil lst))))))))
+)
+
+(defun insert-elem-to-each-pos-aux (elem lst1 lst2)
+    
+    (cond
+        ( ( null lst2) nil)
+        ( t ( (lambda (a)
+                (cons (append (car a) (list elem) (cadr a))
+                    
+                      ( (lambda (x)
+                        (insert-elem-to-each-pos-aux elem (car x) (cadr x)))
+                    
+                       a))
+            )
+                      
+             ( (lambda (lst1 lst2)
+                (list (append lst1 (list (car lst2))) (cdr lst2))
+              
+              ) lst1 lst2)))
+    )
+)
+
+
+(print (permutation '(1 2 3)))
+;>((1 2 3) (1 3 2) (2 1 3) (2 3 1) (3 1 2) (3 2 1))
