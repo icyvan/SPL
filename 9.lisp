c4379ebce3e38e69b2a9c0eed71395fc8b248936
icyvan <43236716+icyvan@users.noreply.github.com>
2019-04-11 10:26:34 +0300

    Update 9.lisp

--- 9.lisp
+++ 9.lisp
@@ -3,20 +3,24 @@
 
 
 (defun even-odd (lst)
-    (cond
-        ((null (car lst)) lst)
-        (t
-         (setq two-lst (even-odd (cddr lst)))
-         (list
-          (cons (car lst) (car two-lst))
-          (cons (cadr lst) (cadr two-lst))
-          )
-         )
-        )
+    ( 
+         (lambda (first-lst second-lst rest) 
+             (
+                 cond
+                    ( (null lst) (list NIL NIL))
+                    ((null second-lst) (list (list first-lst) NIL))
+                    (T (list 
+                            (cons first-lst (car (even-odd rest))) 
+                            (cons second-lst (cadr (even-odd rest))) 
+                       )
+                    )
+             )
+        ) (car lst) (cadr lst) (cddr lst)
     )
+)
 
 (print (even-odd `(1 2 3 4 5 6 )))
 ;>((1 3 5) (2 4 6)) 
 
 (print (even-odd `(q w e r t y)))
-;>((Q E T) (W R Y)) 
+;>((Q E T) (W R Y))
