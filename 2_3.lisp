9d746ad12c3f2cd3c5222fd6ed503df845815ed2
icyvan <43236716+icyvan@users.noreply.github.com>
2019-04-25 00:33:10 +0300

    Update 2_3.lisp

--- 2_3.lisp
+++ 2_3.lisp
@@ -3,3 +3,21 @@
 ;к соответствующему элементу списка
 ;x = (x1 x2 ... xn)
 ;и возвращает список, сформированный из результатов
+
+(defun apl-apply (f x)
+	(mapcar 'funcall f x)
+    )
+
+(defun to-add-1 (num)
+	(+ num 1)
+    )
+
+(defun multiply-2 (num)
+	(* num 2)
+    )
+
+(defun subtract-3 (num)
+	(- num 3)
+    )
+
+(print (apl-apply '(to-add-1 multiply-2 subtract-3) '(1 5 2))) ; (2 10 -1)
