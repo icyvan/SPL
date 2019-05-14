b6fa2196217c4b00c6707a44736b03ad2c459b6c
icyvan <43236716+icyvan@users.noreply.github.com>
2019-05-14 23:30:09 +0300

    Update 1.hs

--- Haskell/1.hs
+++ Haskell/1.hs
@@ -1,7 +1,7 @@
 -- 1. Реализовать на языке Haskell функцию вычисления суммы элементов списка
 
 sum_list :: [Int] -> Int
-sum_list (x) = l (+)  x
+sum_list (x) = foldl1  (+)  x
 
 main = print $ sum_list [1 , 2 , 3 , 4]
 -- > 10
