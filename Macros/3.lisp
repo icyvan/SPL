a3bc698e8aa3442cde4a49d025cdf892029967c1
icyvan <43236716+icyvan@users.noreply.github.com>
2019-05-15 01:02:12 +0300

    Create 3.lisp

---
+++ Macros/3.lisp
@@ -0,0 +1,8 @@
+3. Определите лисповскую форму (IF условие p q) в виде макроса
+
+(defmacro myif (condition p q)
+	`(if ,condition ,p ,q)
+)
+
+(print (myif (= 1 1) 't 'nil)) ; => T
+(print (myif (> 1 2) 't 'nil)) ; => NIL
